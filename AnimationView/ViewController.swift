60cca2376ea700d0a041dc8b6ca4079f294358cd
Stanislav <jqc77s@gmail.com>
2019-12-13 20:13:12 +0300

    Initial Commit

---
+++ AnimationView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationView
+//
+//  Created by Станислав on 13.12.2019.
+//  Copyright © 2019 Станислав. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
